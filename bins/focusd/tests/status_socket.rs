3c2cc7a5d3d90a04b866921a9c1320b9acb9fb2f
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-15 16:06:14 +0200

    test(focusd): migrate status socket to typed ipc

--- bins/focusd/tests/status_socket.rs
+++ bins/focusd/tests/status_socket.rs
@@ -1,11 +1,4 @@
-use std::{
-    fs,
-    io::{Read, Write},
-    os::unix::net::UnixStream,
-    path::PathBuf,
-    thread,
-    time::Duration,
-};
+use std::{fs, path::PathBuf, thread, time::Duration};
 
 use focusd::{DaemonState, serve_once};
 
@@ -28,12 +21,7 @@ fn status_request_reports_running_idle_daemon() {
         thread::sleep(Duration::from_millis(10));
     }
 
-    let mut stream = UnixStream::connect(&socket_path).unwrap();
-    stream.write_all(b"status\n").unwrap();
-    stream.shutdown(std::net::Shutdown::Write).unwrap();
-
-    let mut output = String::new();
-    stream.read_to_string(&mut output).unwrap();
+    let output = focusctl::status_at(&socket_path).unwrap();
     server.join().unwrap();
     let _ = fs::remove_file(&socket_path);
 
