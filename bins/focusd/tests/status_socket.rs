4a1e20414edf2da4a513dc32e5302c4da0866621
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-15 03:15:05 +0200

    test(daemon): avoid consuming readiness connection

--- bins/focusd/tests/status_socket.rs
+++ bins/focusd/tests/status_socket.rs
@@ -1,4 +1,11 @@
-use std::{fs, os::unix::net::UnixStream, path::PathBuf, thread, time::Duration};
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    os::unix::net::UnixStream,
+    path::PathBuf,
+    thread,
+    time::Duration,
+};
 
 use focusd::{DaemonState, serve_once};
 
@@ -15,15 +22,20 @@ fn status_request_reports_running_idle_daemon() {
     let server = thread::spawn(move || serve_once(&server_path, DaemonState::Idle).unwrap());
 
     for _ in 0..50 {
-        if UnixStream::connect(&socket_path).is_ok() {
+        if socket_path.exists() {
             break;
         }
         thread::sleep(Duration::from_millis(10));
     }
 
-    let output = focusd::request_status(&socket_path).unwrap();
+    let mut stream = UnixStream::connect(&socket_path).unwrap();
+    stream.write_all(b"status\n").unwrap();
+    stream.shutdown(std::net::Shutdown::Write).unwrap();
+
+    let mut output = String::new();
+    BufReader::new(stream).read_line(&mut output).unwrap();
     server.join().unwrap();
     let _ = fs::remove_file(&socket_path);
 
-    assert_eq!(output, "Focus daemon: running\nState: Idle\n");
+    assert_eq!(output, "Focus daemon: running | State: Idle\n");
 }
