63265c967d87e83fe5e3101d9bdfc9274e6249b0
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-17 20:54:15 +0200

    test(linux): require fail closed PAM rule RED

--- platform/linux/tests/privilege_guard.rs
+++ platform/linux/tests/privilege_guard.rs
@@ -6,3 +6,13 @@ fn production_privilege_guard_is_scoped_to_the_protected_uid() {
 
     assert_eq!(guard.enforced_uid(), 1000);
 }
+
+#[test]
+fn production_privilege_guard_requires_fail_closed_pam_account_rule() {
+    let guard = ProductionPrivilegeGuard::for_uid(1000);
+
+    assert_eq!(
+        guard.required_pam_account_rule(),
+        "account requisite pam_listfile.so item=user sense=deny file=/var/lib/focus/privilege-deny-users onerr=fail"
+    );
+}
