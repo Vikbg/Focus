2abd74589b172b7222d4a37f42ee9d9e1025ec22
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-17 21:15:52 +0200

    test(linux): require privilege UID resolution RED

--- platform/linux/tests/privilege_guard.rs
+++ platform/linux/tests/privilege_guard.rs
@@ -1,4 +1,6 @@
-use focus_linux::{PrivilegeGuardControl, ProductionPrivilegeGuard};
+use focus_linux::{
+    PrivilegeGuardControl, PrivilegeGuardError, ProductionPrivilegeGuard,
+};
 
 fn assert_privilege_control<T: PrivilegeGuardControl>() {}
 
@@ -18,4 +20,11 @@ fn production_privilege_guard_requires_fail_closed_pam_account_rule() {
         guard.required_pam_account_rule(),
         "account requisite pam_listfile.so item=user sense=deny file=/var/lib/focus/privilege-deny-users onerr=fail"
     );
+}
+
+#[test]
+fn unknown_uid_is_rejected_before_privilege_files_are_touched() {
+    let mut guard = ProductionPrivilegeGuard::for_uid(u32::MAX);
+
+    assert_eq!(guard.arm(), Err(PrivilegeGuardError::InvalidUserIdentity));
 }
