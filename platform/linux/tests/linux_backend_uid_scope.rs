ef2b43f6cdf062796e670b28f9196943b94ac4bc
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-17 21:26:04 +0200

    test(linux): require privilege backend UID binding RED

--- platform/linux/tests/linux_backend_uid_scope.rs
+++ platform/linux/tests/linux_backend_uid_scope.rs
@@ -1,9 +1,10 @@
 use focus_linux::LinuxBackend;
 
 #[test]
-fn production_backend_binds_process_control_and_guard_to_the_same_uid() {
+fn production_backend_binds_every_user_scoped_guard_to_the_same_uid() {
     let backend = LinuxBackend::for_uid(1000);
 
     assert_eq!(backend.process_control().enforced_uid(), Some(1000));
     assert_eq!(backend.process_guard().enforced_uid(), Some(1000));
+    assert_eq!(backend.privilege_guard().enforced_uid(), 1000);
 }
