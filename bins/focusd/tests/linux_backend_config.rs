818b9129b6b47c5a4dc1591343f0d116667dc755
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-17 04:20:37 +0200

    test(focusd): bind backend construction to runtime config

--- bins/focusd/tests/linux_backend_config.rs
+++ bins/focusd/tests/linux_backend_config.rs
@@ -1,10 +1,10 @@
-use focusd::{RuntimeConfig, linux_backend_from_config};
+use focusd::RuntimeConfig;
 
 #[test]
 fn runtime_config_uid_builds_uid_scoped_linux_backend() {
     let config = RuntimeConfig::from_values(Some("1000"), None).unwrap();
 
-    let backend = linux_backend_from_config(&config);
+    let backend = config.linux_backend();
 
     assert_eq!(backend.process_control().enforced_uid(), Some(1000));
     assert_eq!(backend.process_guard().enforced_uid(), Some(1000));
