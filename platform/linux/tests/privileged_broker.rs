4a8d0b03e81e7b1aac2ae55855b65fbe3cb20751
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-18 22:14:07 +0200

    test(linux): define safe typed broker RED

--- platform/linux/tests/privileged_broker.rs
+++ platform/linux/tests/privileged_broker.rs
@@ -7,8 +7,9 @@ use focus_platform::PrivilegedAction;
 #[derive(Debug, Default)]
 struct RecordingDockerControl {
     trusted: bool,
-    fail_start: bool,
+    fail_stop: bool,
     starts: usize,
+    stops: usize,
 }
 
 impl DockerServiceControl for RecordingDockerControl {
@@ -18,7 +19,12 @@ impl DockerServiceControl for RecordingDockerControl {
 
     fn start_docker(&mut self) -> Result<(), PrivilegeBrokerError> {
         self.starts += 1;
-        if self.fail_start {
+        Ok(())
+    }
+
+    fn stop_docker(&mut self) -> Result<(), PrivilegeBrokerError> {
+        self.stops += 1;
+        if self.fail_stop {
             Err(PrivilegeBrokerError::ActionFailed)
         } else {
             Ok(())
@@ -27,15 +33,32 @@ impl DockerServiceControl for RecordingDockerControl {
 }
 
 #[test]
-fn docker_start_routes_to_one_typed_docker_control_action() {
+fn docker_stop_routes_to_one_typed_docker_control_action() {
     let control = RecordingDockerControl {
         trusted: true,
         ..RecordingDockerControl::default()
     };
     let mut broker = LinuxPrivilegeBroker::new(control);
 
-    assert_eq!(broker.execute(PrivilegedAction::DockerStart), Ok(()));
-    assert_eq!(broker.control().starts, 1);
+    assert_eq!(broker.execute(PrivilegedAction::DockerStop), Ok(()));
+    assert_eq!(broker.control().stops, 1);
+    assert_eq!(broker.control().starts, 0);
+}
+
+#[test]
+fn rootful_docker_start_is_not_approved_by_the_task21_broker() {
+    let control = RecordingDockerControl {
+        trusted: true,
+        ..RecordingDockerControl::default()
+    };
+    let mut broker = LinuxPrivilegeBroker::new(control);
+
+    assert_eq!(
+        broker.execute(PrivilegedAction::DockerStart),
+        Err(PrivilegeBrokerError::ActionNotApproved)
+    );
+    assert_eq!(broker.control().starts, 0);
+    assert_eq!(broker.control().stops, 0);
 }
 
 #[test]
@@ -44,26 +67,27 @@ fn untrusted_privilege_executor_fails_before_any_action() {
     let mut broker = LinuxPrivilegeBroker::new(control);
 
     assert_eq!(
-        broker.execute(PrivilegedAction::DockerStart),
+        broker.execute(PrivilegedAction::DockerStop),
         Err(PrivilegeBrokerError::UnsafeExecutor)
     );
-    assert_eq!(broker.control().starts, 0);
+    assert_eq!(broker.control().stops, 0);
 }
 
 #[test]
 fn privileged_action_failure_is_never_reported_as_success() {
     let control = RecordingDockerControl {
         trusted: true,
-        fail_start: true,
+        fail_stop: true,
         starts: 0,
+        stops: 0,
     };
     let mut broker = LinuxPrivilegeBroker::new(control);
 
     assert_eq!(
-        broker.execute(PrivilegedAction::DockerStart),
+        broker.execute(PrivilegedAction::DockerStop),
         Err(PrivilegeBrokerError::ActionFailed)
     );
-    assert_eq!(broker.control().starts, 1);
+    assert_eq!(broker.control().stops, 1);
 }
 
 #[test]
