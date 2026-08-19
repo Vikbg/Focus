e64e6d2bb232c36d309a61c990257a98cc5edf38
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-20 01:05:40 +0200

    feat(linux): export WireGuard VPN adapter

--- platform/linux/src/lib.rs
+++ platform/linux/src/lib.rs
@@ -26,6 +26,7 @@ mod rustix_pidfd;
 mod system_cgroup_control;
 mod system_ebpf_class_program;
 mod systemd;
+mod wireguard_vpn;
 
 pub use backend::{LinuxBackend, ProductionLinuxBackend};
 pub use cgroup_process_class::{
@@ -95,6 +96,7 @@ pub use production_privilege_guard::ProductionPrivilegeGuard;
 pub use rustix_pidfd::RustixPidfdOps;
 pub use system_cgroup_control::{FOCUS_CGROUP_ROOT, SystemCgroupControl};
 pub use system_ebpf_class_program::{FOCUS_EBPF_OBJECT_PATH, SystemEgressClassProgramControl};
+pub use wireguard_vpn::{WireGuardCommandControl, WireGuardProfile, WireGuardVpnActionControl};
 
 use std::{
     error::Error,
