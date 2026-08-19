3623c2beb671819cca58bcc77e10f00e1f9ce718
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-19 14:09:51 +0200

    style(linux): format eBPF egress policy tests

--- platform/linux/tests/ebpf_egress_policy.rs
+++ platform/linux/tests/ebpf_egress_policy.rs
@@ -4,16 +4,10 @@ use focus_linux::{EgressProtocol, EgressRuleError, Ipv4EgressRule};
 
 #[test]
 fn daemon_rule_encoding_is_stable_and_protocol_aware() {
-    let tcp = Ipv4EgressRule::new(Ipv4Addr::new(127, 0, 0, 1), 8080, EgressProtocol::Tcp)
-        .unwrap();
-    let udp = Ipv4EgressRule::new(Ipv4Addr::new(127, 0, 0, 1), 8080, EgressProtocol::Udp)
-        .unwrap();
-    let other_port = Ipv4EgressRule::new(
-        Ipv4Addr::new(127, 0, 0, 1),
-        8081,
-        EgressProtocol::Tcp,
-    )
-    .unwrap();
+    let tcp = Ipv4EgressRule::new(Ipv4Addr::new(127, 0, 0, 1), 8080, EgressProtocol::Tcp).unwrap();
+    let udp = Ipv4EgressRule::new(Ipv4Addr::new(127, 0, 0, 1), 8080, EgressProtocol::Udp).unwrap();
+    let other_port =
+        Ipv4EgressRule::new(Ipv4Addr::new(127, 0, 0, 1), 8081, EgressProtocol::Tcp).unwrap();
 
     assert_eq!(tcp.protocol_number(), 6);
     assert_eq!(udp.protocol_number(), 17);
