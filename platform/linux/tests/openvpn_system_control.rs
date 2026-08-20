35e7a7e66c436fb9d892a56b660dee4694c980a1
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-20 19:04:31 +0200

    test(linux): require typed OpenVPN unit command contract

--- platform/linux/tests/openvpn_system_control.rs
+++ platform/linux/tests/openvpn_system_control.rs
@@ -1,6 +1,11 @@
-use std::{fs, path::PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
-use focus_linux::{OpenVpnCommandControl, OpenVpnUnitName, SystemOpenVpnCommandControl};
+use focus_linux::{
+    OpenVpnCommandControl, OpenVpnUnitName, PrivilegeBrokerError, SystemOpenVpnCommandControl,
+};
 
 fn repo_root() -> PathBuf {
     PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../..")
@@ -8,10 +13,40 @@ fn repo_root() -> PathBuf {
 
 fn assert_openvpn_command_control<T: OpenVpnCommandControl + Default>() {}
 
+#[derive(Default)]
+struct TypedUnitContract;
+
+impl OpenVpnCommandControl for TypedUnitContract {
+    fn executor_is_trusted(&self) -> Result<bool, PrivilegeBrokerError> {
+        Ok(true)
+    }
+
+    fn config_is_trusted(&self, _config: &Path) -> Result<bool, PrivilegeBrokerError> {
+        Ok(true)
+    }
+
+    fn start_service(
+        &mut self,
+        _unit: &OpenVpnUnitName,
+        _config: &Path,
+    ) -> Result<(), PrivilegeBrokerError> {
+        Ok(())
+    }
+
+    fn stop_service(&mut self, _unit: &OpenVpnUnitName) -> Result<(), PrivilegeBrokerError> {
+        Ok(())
+    }
+}
+
 #[test]
 fn production_openvpn_control_implements_narrow_command_contract() {
     assert_openvpn_command_control::<SystemOpenVpnCommandControl>();
     let _control = SystemOpenVpnCommandControl::default();
+}
+
+#[test]
+fn openvpn_command_contract_uses_typed_unit_identity() {
+    assert_openvpn_command_control::<TypedUnitContract>();
 }
 
 #[test]
