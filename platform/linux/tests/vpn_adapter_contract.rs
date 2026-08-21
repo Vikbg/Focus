30442a547d4d887f49f144ef28076ab2e0eda544
viktor_srhk <viktorserhiienko12@gmail.com>
2026-08-21 02:26:03 +0200

    test(linux): cover OpenVPN provider-neutral adapter contract

--- platform/linux/tests/vpn_adapter_contract.rs
+++ platform/linux/tests/vpn_adapter_contract.rs
@@ -1,8 +1,8 @@
 use std::path::{Path, PathBuf};
 
 use focus_linux::{
-    PrivilegeBrokerError, VpnAdapter, WireGuardCommandControl, WireGuardProfile,
-    WireGuardVpnActionControl,
+    OpenVpnAdapter, OpenVpnCommandControl, OpenVpnProfile, OpenVpnUnitName, PrivilegeBrokerError,
+    VpnAdapter, WireGuardCommandControl, WireGuardProfile, WireGuardVpnActionControl,
 };
 
 #[derive(Debug, Default)]
@@ -29,6 +29,35 @@ impl WireGuardCommandControl for RecordingWireGuardCommandControl {
     }
 }
 
+#[derive(Debug, Default)]
+struct RecordingOpenVpnCommandControl {
+    starts: Vec<(String, PathBuf)>,
+}
+
+impl OpenVpnCommandControl for RecordingOpenVpnCommandControl {
+    fn executor_is_trusted(&self) -> Result<bool, PrivilegeBrokerError> {
+        Ok(true)
+    }
+
+    fn config_is_trusted(&self, _config: &Path) -> Result<bool, PrivilegeBrokerError> {
+        Ok(true)
+    }
+
+    fn start_service(
+        &mut self,
+        unit: &OpenVpnUnitName,
+        config: &Path,
+    ) -> Result<(), PrivilegeBrokerError> {
+        self.starts
+            .push((unit.as_str().to_owned(), config.to_path_buf()));
+        Ok(())
+    }
+
+    fn stop_service(&mut self, _unit: &OpenVpnUnitName) -> Result<(), PrivilegeBrokerError> {
+        Ok(())
+    }
+}
+
 fn assert_vpn_adapter<T: VpnAdapter>() {}
 
 #[test]
@@ -44,6 +73,25 @@ fn wireguard_implements_provider_neutral_vpn_adapter_contract() {
     assert_eq!(adapter.command_control().ups, vec![config]);
     assert_eq!(
         adapter.connect(99),
+        Err(PrivilegeBrokerError::ActionNotApproved)
+    );
+}
+
+#[test]
+fn openvpn_implements_provider_neutral_vpn_adapter_contract() {
+    assert_vpn_adapter::<OpenVpnAdapter<RecordingOpenVpnCommandControl>>();
+
+    let config = PathBuf::from("/etc/focus/openvpn/study.ovpn");
+    let profile = OpenVpnProfile::new(51, config.clone());
+    let mut adapter = OpenVpnAdapter::new([profile], RecordingOpenVpnCommandControl::default());
+
+    assert_eq!(VpnAdapter::connect(&mut adapter, 51), Ok(()));
+    assert_eq!(
+        adapter.command_control().starts,
+        vec![("focus-openvpn-51.service".to_owned(), config)]
+    );
+    assert_eq!(
+        VpnAdapter::connect(&mut adapter, 99),
         Err(PrivilegeBrokerError::ActionNotApproved)
     );
 }
